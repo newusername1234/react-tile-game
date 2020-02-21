39dd71656858e44e4f176c56118a456c6ce4a9dd
Wes <wesleyayer92@gmail.com>
2020-02-21 11:04:35 -0500

    adds board styles

--- src/index.js
+++ src/index.js
@@ -11,102 +11,80 @@ function Square({ style }) {
         />
     );
 }
-
+// add working reset button or add step back w/ redux
 function DPad({onClick}) {
     return (
         <div className="dpad">
-            <LeftButton onClick={onClick}/>
-            <UpButton onClick={onClick}/>
-            <RightButton onClick={onClick}/>
-            <DownButton onClick={onClick}/>
+            <button value="left" onClick={onClick}>left</button>
+            <button value="right" onClick={onClick}>right</button>
+            <button value="up" onClick={onClick}>up</button>
+            <button value="down" onClick={onClick}>down</button>
+            {/* <button value="reset" onClick={onClick}>reset</button> */}
         </div>
-    );
-}
-
-function LeftButton({onClick}) {
-    return (
-        <button
-            value="left"
-            onClick={onClick}
-        >left
-        </button>
-    );
-}
-
-function UpButton({onClick}) {
-    return (
-        <button
-            value="up"
-            onClick={onClick}
-        >up
-        </button>
-    );
-}
-
-function RightButton({onClick}) {
-    return (
-        <button
-            value="right"
-            onClick={onClick}
-        >right
-        </button>
-    );
-}
-
-function DownButton({onClick}) {
-    return (
-        <button
-            value="down"
-            onClick={onClick}
-        >down
-        </button>
     );
 }
     
 class Board extends React.Component {
-    
-    renderSquare(i, j) {
+    renderRow(x, y) {
+        // console.log('board coords: ' + x + ', ' + y);
+        // console.log('state coords: ' + this.props.xCoords + ', ' + this.props.yCoords);
         let myStyle;
-        this.props.squareState[i][j] ? 
-            myStyle = {backgroundColor: 'black'} :
-            myStyle = {backgroundColor: 'white'};
-        return (
-            <Square
-                style={myStyle}
-            />
-        );
+        const myArr = [];   
+        for (y; y < 11; y++) {
+            switch(this.props.squares[x][y]) {
+                case '':
+                    myStyle = {backgroundColor: 'lightblue', border: '3px solid cornflowerblue'};
+                    break;
+                case 'X':
+                    myStyle = {backgroundColor: 'grey', border: '3px solid black'};
+                    break;
+                case 'O':
+                    x === this.props.yCoords && y === this.props.xCoords ? 
+                        myStyle = {backgroundColor: 'yellow', border: '3px solid orange'} :
+                        myStyle = {backgroundColor: 'rgb(224, 68, 68)', border: '3px solid rgb(151, 40, 40)'};
+                    break;
+                default:
+                    break;
+            }
+            // this.props.squares[x][y] ? 
+            //     myStyle = {backgroundColor: 'black'} :
+            //     myStyle = {backgroundColor: 'white'};
+                myArr.push(<Square key={y} style={myStyle}/>);
+        }
+        return myArr;
     }
-            
-    render() {
-        const winCheck = calculateWinner(this.props.squareState);
 
+    render() {
+        const winCheck = calculateWinner(this.props.squares);
+        // const loseCheck = calculateLoser(this.props.squares, this.props.xCoords, this.props.yCoords);
         return (
             <div>
                 <div className="board-row">
-                    {this.renderSquare(0, 0)}
-                    {this.renderSquare(0, 1)}
-                    {this.renderSquare(0, 2)}
-                    {this.renderSquare(0, 3)}
+                    {this.renderRow(0, 0)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(1, 0)}
-                    {this.renderSquare(1, 1)}
-                    {this.renderSquare(1, 2)}
-                    {this.renderSquare(1, 3)}
+                    {this.renderRow(1, 0)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(2, 0)}
-                    {this.renderSquare(2, 1)}
-                    {this.renderSquare(2, 2)}
-                    {this.renderSquare(2, 3)}
+                    {this.renderRow(2, 0)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(3, 0)}
-                    {this.renderSquare(3, 1)}
-                    {this.renderSquare(3, 2)}
-                    {this.renderSquare(3, 3)}
+                    {this.renderRow(3, 0)}
                 </div>
-                {winCheck && <h1>You won!</h1>}
+                <div className="board-row">
+                    {this.renderRow(4, 0)}
+                </div>
+                <div className="board-row">
+                    {this.renderRow(5, 0)}
+                </div>
+                <div className="board-row">
+                    {this.renderRow(6, 0)}
+                </div>
+                <div className="board-row">
+                    {this.renderRow(7, 0)}
+                </div>
+                {winCheck && <h1>Winrar!</h1>}
+                {/* {loseCheck && <h1>You suck at puzzles</h1>} */}
             </div>
         );
     }
@@ -117,50 +95,52 @@ class Game extends React.Component {
         super(props);
         this.state = {
             squares: [
-                ['filled', '', '', ''],
-                ['', '', '', ''],
-                ['', '', '', ''],
-                ['', '', '', ''],
+                ['', 'X', 'X', '', '', 'O', '', '', '', '', ''],
+                ['', '', '', '', '', 'X', '', '', '', 'X', ''],
+                ['X', '', '', '', '', '', '', 'X', '', '', ''],
+                ['', '', 'X', '', '', '', '', '', '', '', ''],
+                ['', '', 'X', '', '', '', 'X', '', '', '', ''],
+                ['X', '', '', '', 'X', '', '', '', '', '', 'X'],
+                ['', '', '', '', '', '', '', '', 'X', '', ''],
+                ['', '', '', '', '', '', 'X', '', '', '', '']
             ],
-            xCoords: 0,
+            xCoords: 5,
             yCoords: 0
         };
     }
 
     handleClick = (event) => {
-        const squares = [...this.state.squares];
-        let xCoords = this.state.xCoords;
-        let yCoords = this.state.yCoords;
+        let squares = [...this.state.squares];
+        let x = this.state.xCoords;
+        let y = this.state.yCoords;
 
         switch(event.target.value) {
             case 'left':
-                if (xCoords > 0) {xCoords -= 1};
+                if (x > 0) {x -= 1};
                 break;
             case 'right':
-                if (xCoords < squares[0].length - 1) {xCoords += 1};
+                if (x < squares[0].length - 1) {x += 1};
                 break;
             case 'up':
-                if (yCoords > 0) {yCoords -= 1};
+                if (y > 0) {y -= 1};
                 break;
             case 'down':
-                if (yCoords < squares.length - 1) {yCoords += 1};
+                if (y < squares.length - 1) {y += 1};
                 break;
             default:
                 break;
         };
 
-        if (squares[yCoords][xCoords]) {
+        if (squares[y][x]) {
             return;
         }
 
-        squares[yCoords][xCoords] = 'filled';
+        squares[y][x] = 'O';
 
-
-        // console.log(xCoords + ', ' + yCoords);
         this.setState({
             squares,
-            xCoords,
-            yCoords
+            xCoords: x,
+            yCoords: y
         }, () => console.table(this.state));
     }
     
@@ -169,7 +149,7 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board squareState={this.state.squares}/>
+                    <Board squares={this.state.squares} xCoords={this.state.xCoords} yCoords={this.state.yCoords}/>
                     <DPad onClick={this.handleClick}/>
                 </div>
             </div>
@@ -180,13 +160,20 @@ class Game extends React.Component {
 function calculateWinner(squares) {
     for (let array of squares) {
         for (let item of array) {
-            if (item !== 'filled') {
+            if (item === '') {
                 return false;
             }
         }
     }
     return true;
 }
+
+// function calculateLoser(squares, x, y) {
+//     if (squares[y + 1][x] && squares[y - 1][x] && squares[y][x + 1] && squares[y][x - 1]) {
+//         return true;
+//     }
+//     return false;
+// }
 
 // ===============================================
 
